80abe52115657d4eb635bd4c00a5477cd60fb868
Cristhian Rodríguez <crisrodam1996@gmail.com>
2026-03-28 14:18:00 -0500

    Registro de un usuario

--- src/data/seed.data.js
+++ src/data/seed.data.js
@@ -1,16 +1,9 @@
 export const listaUsuarios = [
   {
-    nombresCompletos: 'Usuario Admin',
+    nombresCompletos: 'Super Administrador',
     cedula: '0940501596',
     telefono: '0967148226',
     correo: 'crisrodam1996@gmail.com',
     rol: 'Administrador',
-  },
-  {
-    nombresCompletos: 'Usuario Contador',
-    cedula: '0950401596',
-    telefono: '0982962441',
-    correo: 'cristhian.rodriguez@utc.edu.ec',
-    rol: 'Contador',
   },
 ]
